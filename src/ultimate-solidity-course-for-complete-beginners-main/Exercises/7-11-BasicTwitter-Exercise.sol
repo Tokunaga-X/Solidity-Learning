14807c3cc296dae380a25579bea784b26d5b3c20
TokunagaX <officialxyc@gmail.com>
2024-07-16 00:52:41 +0800

    finish exercise 7

--- src/ultimate-solidity-course-for-complete-beginners-main/Exercises/7-11-BasicTwitter-Exercise.sol
+++ src/ultimate-solidity-course-for-complete-beginners-main/Exercises/7-11-BasicTwitter-Exercise.sol
@@ -2,13 +2,26 @@
 
 pragma solidity ^0.8.0;
 
-// 1️⃣ Create a Twitter Contract 
-// 2️⃣ Create a mapping between user and tweet 
+// 1️⃣ Create a Twitter Contract
+// 2️⃣ Create a mapping between user and tweet
 // 3️⃣ Add function to create a tweet and save it in mapping
-// 4️⃣ Create a function to get Tweet 
-// 5️⃣ Add array of tweets 
+// 4️⃣ Create a function to get Tweet
+// 5️⃣ Add array of tweets
 
 contract Twitter {
+    mapping(address => string[]) public tweets;
 
+    function createTweet(string memory _tweets) public {
+        tweets[msg.sender].push(_tweets);
+    }
 
+    function getTweet(
+        address targetAddress
+    ) public view returns (string[] memory) {
+        return tweets[targetAddress];
+    }
+
+    function clearTweet(address targetAddress) public {
+        tweets[targetAddress] = [""];
+    }
 }
