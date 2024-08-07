05c8290a2b489dff40c75604418f2589e4c6420b
TokunagaX <officialxyc@gmail.com>
2024-08-08 01:41:34 +0800

    update hardhat.config.ts file

--- ERC20-Token-Learn/hardhat.config.ts
+++ ERC20-Token-Learn/hardhat.config.ts
@@ -5,7 +5,7 @@ const { vars } = require("hardhat/config");
 
 // Go to https://infura.io, sign up, create a new API key
 // in its dashboard, and add it to the configuration variables
-const INFURA_API_KEY = '';
+const INFURA_API_KEY = vars.get("INFURA_API_KEY");
 
 // Add your Sepolia account private key to the configuration variables
 // To export your private key from Coinbase Wallet, go to
@@ -13,7 +13,7 @@ const INFURA_API_KEY = '';
 // To export your private key from Metamask, open Metamask and
 // go to Account Details > Export Private Key
 // Beware: NEVER put real Ether into testing accounts
-const SEPOLIA_PRIVATE_KEY = '';
+const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY");
 
 module.exports = {
   solidity: "0.8.24",
